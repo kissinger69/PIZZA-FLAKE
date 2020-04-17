efb5cebc593c728491277068d7eb5ff5d390977e
kissinger69 <kissyoa69@gmail.com>
2020-04-18 00:45:26 +0300

    lightened background photo

---
+++ js/pizza.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    var toppingsPrices={Pepperoni: 200, Onions: 100, Bacon: 100, Pineapple: 100};
+    var crustPrices={crispy: 100,stuffed: 200, glutten_free: 300 }
+
+    $.each(toppingsPrices,function(k,v) {
+        var option ='<option value="' + k + '">' + k + '@' + v + 'Ksh</option>';
+    }
+}
